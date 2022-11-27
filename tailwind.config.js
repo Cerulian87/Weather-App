c61e3ba37b46037693557d0fbd51e596ce9fda32
Johnathon <johnathon.d.martin@gmail.com>
2022-11-27 11:25:02 -0700

    Added a comments

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      // backgroundImage: {
+      //   'hotImage': "url('/src/images/test.png')",  //test image for background
+      // }
+    },
+  },
+  plugins: [],
+};
